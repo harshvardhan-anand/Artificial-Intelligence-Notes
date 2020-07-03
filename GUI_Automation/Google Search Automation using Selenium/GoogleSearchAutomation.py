5c5badb7ce599eb78296f25be555851c010ac3e7
Harshvardhan Anand <hharshvardhanaanand@gmail.com>
2020-07-03 10:53:29 +0530

    Google Search  automation updated

--- GUI_Automation/Google Search Automation using Selenium/GoogleSearchAutomation.py
+++ GUI_Automation/Google Search Automation using Selenium/GoogleSearchAutomation.py
@@ -21,15 +21,26 @@ driver.get('https://google.com')
 # Finding the search bar to send the key strokes.
 search_bar = driver.find_element_by_xpath(
                     '//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
-def search():
-    query = input("Google: Please give your query\n\nYou: ")
-    # This method is used to send the key stroke to the browser.
-    search_bar.send_keys(query)
 
-    # Pressing Enter key
-    search_bar.send_keys(Keys.ENTER)
-search()
+# Start an new tab
+# driver.execute_script("window.open('https://www.facebook.com/groups/2395385783831009','_blank');")
+# [1] means new_tab 1
+# driver.switch_to.window(driver.window_handles[1])
+# For Fullscreen
+# driver.fullscreen_window()
+
+# def search():
+#     query = input("Google: Please give your query\n\nYou: ")
+#     # This method is used to send the key stroke to the browser.
+#     search_bar.send_keys(query)
+#     search_bar.send_keys(Keys.CONTROL, 't')
+#     # Pressing Enter key
+#     search_bar.send_keys(Keys.ENTER)
+
+# search()
 if not(input("Press enter to exit")):
     # time.sleep(60)
+    # driver.close will only close the tab at index 0
+    # https://www.edureka.co/community/52772/close-active-current-without-closing-browser-selenium-python
     driver.close()
 
